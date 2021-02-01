f028822fc6623c2571489ecbcaab76f736bfda46
Qingyi <qingyi.tss@gmail.com>
2021-02-01 17:13:37 +0800

    Use USTC mirror instead of Tuna; Explicit set $LANG to en_US.UTF-8.

--- fish/config.fish
+++ fish/config.fish
@@ -4,6 +4,8 @@
 set -gx PATH $HOME/.cargo/bin $PATH
 set -gx PATH $HOME/go/bin $PATH
 
+set -gx LANG en_US.UTF-8
+
 # golang environments
 set -gx GOPATH $HOME/go
 set -gx GOPROXY "https://goproxy.cn"
@@ -11,11 +13,14 @@ set -gx GO111MODULE "on"
 alias go4linux="GOOS=linux GOARCH=386 CGO_ENABLED=0 go build"
 
 # set rustup and rust mirrors
-set -gx RUSTUP_DIST_SERVER https://mirrors.tuna.tsinghua.edu.cn/rustup
+set -gx RUSTUP_DIST_SERVER https://mirrors.ustc.edu.cn/rust-static
 
 # homebrew bottle mirrors
-set -gx HOMEBREW_BOTTLE_DOMAIN https://mirrors.tuna.tsinghua.edu.cn/homebrew-bottles
+set -gx HOMEBREW_BOTTLE_DOMAIN https://mirrors.ustc.edu.cn/homebrew-bottles
 
+# Usually, I start a emacs server with emacs app, so only emacsclient
+# needed in terminal
+alias emacs="emacsclient"
 
 # correction for common mistake
 alias t="tmux"
