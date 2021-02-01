73826954711b814588e9d33e31f899134e4c1bd1
Qingyi <qingyi.tss@gmail.com>
2021-02-01 18:24:56 +0800

    Add functions to work with emacs vterm.

---
+++ fish/functions/vterm_printf.fish
@@ -0,0 +1,13 @@
+# function to send information to emacs vterm
+
+function vterm_printf;
+    if [ -n "$TMUX" ]
+        # tell tmux to pass the escape sequences through
+        printf "\ePtmux;\e\e]%s\007\e\\" "$argv"
+    else if string match -q -- "screen*" "$TERM"
+        # GNU screen (screen, screen-256color, screen-256color-bce)
+        printf "\eP\e]%s\007\e\\" "$argv"
+    else
+        printf "\e]%s\e\\" "$argv"
+    end
+end
