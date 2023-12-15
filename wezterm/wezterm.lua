6d43329e29256d6ac4fea3d9fe233da3e8acc339
Yi Qing <qingyi.tss@gmail.com>
2023-12-15 17:13:25 +0800

    change wezterm theme with keybinding.

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -8,16 +8,16 @@ if wezterm.config_builder then
 end
 
 config.font = wezterm.font_with_fallback({
-    -- { family = "Iosevka Term Extended", weight = "Regular", harfbuzz_features = { "ss15" } },
-    -- { family = "Monego", weight = "Regular" },
-    -- { family = "Cascadia Code", weight = "Light", harfbuzz_features = { "calt", "ss01" } },
-    { family = "JetBrains Mono", weight = "Regular" },
+    -- { family = "Iosevka Term Extended",  weight = "Regular", harfbuzz_features = { "ss15" } },
+    -- { family = "Cascadia Code",          weight = "Light",   harfbuzz_features = { "calt", "ss01" } },
+    { family = "Monego",                 weight = "Regular" },
+    { family = "JetBrains Mono",         weight = "Regular" },
 
-    { family = "LXGW WenKai", weight = "Regular", scale = 1.2 },
+    { family = "LXGW WenKai",            weight = "Regular", scale = 1.2 },
     { family = "Symbols Nerd Font Mono", scale = 0.75 },
 })
 
-config.font_size = 13
+config.font_size = 12.5
 config.freetype_load_target = "Light"
 config.front_end = "WebGpu"
 config.freetype_load_flags = "NO_AUTOHINT"
@@ -29,9 +29,9 @@ config.freetype_load_flags = "NO_AUTOHINT"
 -- config.cursor_blink_ease_in = "Constant"
 -- config.cursor_blink_ease_out = "Constant"
 -- config.cursor_blink_rate = 800
-config.force_reverse_video_cursor = true
+config.force_reverse_video_cursor = true -- change cursor fg base on bg.
 config.mouse_bindings = {
-    -- make CTRL-Click open hyperlinks
+    -- make CTRL-Click open hyperlinks, not working in tmux.
     {
         event = { Up = { streak = 1, button = 'Left' } },
         mods = 'CTRL',
@@ -43,22 +43,41 @@ config.mouse_bindings = {
 config.send_composed_key_when_left_alt_is_pressed = false;
 config.send_composed_key_when_right_alt_is_pressed = false;
 
--- Config theme, better to use a custom cursor color if using theme, some theme
--- override the cursor color and may cover the text color in neovim.
---
--- config.color_scheme = "Modus-Vivendi"
--- config.color_scheme = "Tomorrow Night"
--- config.color_scheme = "Solarized Dark (Gogh)"
+-- change theme with keys
+wezterm.on("flip-colorscheme", function(window, _)
+    local colorschemes = {
+        dark = "Modus-Vivendi",
+        -- dark = "Tomorrow Night",
+        -- dark = "Catppuccin Mocha",
 
-config.window_background_opacity = 0.85
-config.macos_window_background_blur = 20
-config.window_padding = { left = "0.5cell", right = "0.5cell", top = 0, bottom = 0 }
+        light = "Catppuccin Latte",
+        -- light = "Modus-Operandi",
+    }
 
+    local overrides = window:get_config_overrides()
+    local selected = ""
+
+    if not overrides or overrides.color_scheme == colorschemes.dark then
+        selected = colorschemes.light
+    else
+        selected = colorschemes.dark
+    end
+    window:set_config_overrides {
+        color_scheme = selected
+    }
+end)
+
+-- config.window_background_opacity = 0.85
+-- config.macos_window_background_blur = 20
+-- config.window_decorations = "INTEGRATED_BUTTONS|RESIZE"
+config.window_padding = { left = 5, right = 5, top = 5, bottom = 0 }
 config.hide_tab_bar_if_only_one_tab = true
+
+config.automatically_reload_config = false
 
 config.keys = {
     { mods = "CMD|SHIFT", key = "r", action = wezterm.action.ReloadConfiguration },
+    { mods = "CMD|SHIFT", key = "t", action = wezterm.action.EmitEvent "flip-colorscheme" },
 }
--- config.automatically_reload_config = true
 
 return config
