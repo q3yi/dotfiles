6f29bb2c18ee1c405ac5c9296be3a35b5bc3e7b9
Yi Qing <qingyi.tss@gmail.com>
2024-01-17 10:23:22 +0800

    support search different projects in p command.

--- fish/functions/p.fish
+++ fish/functions/p.fish
@@ -1,7 +1,27 @@
 # Find and jump to project folder in predinfined roots
 
 function p --description 'Jump to git project folder'
+    set --local options h/help 'has=+'
+
+    argparse $options -- $argv
+
+    if set --query _flag_help
+        printf "Usage: p [OPTIONS] [FOLDERS]\n\n"
+        printf "Options:\n"
+        printf "  -h/--help           Prints help and exits\n"
+        printf "  --has [SEARCH_FILE] Has file in folder(default is .git)\n"
+        return 0
+    end
+
+    set --query _flag_has; or set --local _flag_has ".git"
+
+    set --local folders $REPOS
+
+    if test -n "$argv"
+        set folders $argv
+    end
+
     # or use fd command instead of fdir
     # fd '.git$' $root --prune -u -t d -d 3 -x echo {//}
-    cd $(fdir $REPOS | fzf)
+    cd $(fdir --has $_flag_has $folders | fzf)
 end
