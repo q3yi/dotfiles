8bd80d57e9c363e8731f2cbd55052f0cbc16575b
Qing Yi <qing.yi@outlook.com>
2024-11-11 12:53:34 +0800

    add a function to source shell env file in fish.

---
+++ fish/functions/envsource.fish
@@ -0,0 +1,19 @@
+# helper function to source bash sytle env file
+
+function envsource --description "Source env file."
+
+  set -f envfile "$argv"
+  if not test -f "$envfile"
+    echo "Unable to load $envfile"
+    return 1
+  end
+
+  while read line
+    if not string match -qr '^#|^$' "$line"
+      set item (string split -m 1 '=' $line)
+      set -gx $item[1] $item[2]
+      echo "Exported key $item[1]"
+    end
+  end < "$envfile"
+
+end
