64f4a5ab68d07a9981aa5fad366e3213988599de
Qing Yi <qing.yi@outlook.com>
2026-07-29 17:59:46 +0800

    add zmx config to starship and fish shell.

--- fish/functions/fish_title.fish
+++ fish/functions/fish_title.fish
@@ -1,11 +1,7 @@
-
 function fish_title
-
-    # emacs is basically the only term that can't handle it.
-    if not set -q INSIDE_EMACS
-        echo (status current-command) (__fish_pwd)
-    else if test "$INSIDE_EMACS" = "vterm"
-        # use vterm inside emacs
+    if set -q ZMX_SESSION
+        echo "[$ZMX_SESSION"] (status current-command) (prompt_pwd)
+    else
         echo (status current-command) (prompt_pwd)
     end
 end
